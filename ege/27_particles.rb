d95be906cda6c946cf5ef08e33caf0ee5709e25d
Maxim Tyulenev <mr_makss@mail.ru>
2018-05-26 01:19:03 +0700

    ref

--- ege/27_particles.rb
+++ ege/27_particles.rb
@@ -5,14 +5,14 @@
 # Вы можете записать решение на любом языке
 # Обратите внимание на print
 
-number_speeds = gets.to_i
 lower_limiter = 1000000001 #10**9 + 1
 k = j = c = 0;
-(1..number_speeds).each { |i|
+(1..gets.to_i).each { |i|
   (speed = gets.to_i) % 2 != 0 ? (c += 1; ((lower_limiter = speed; k = i) if speed < lower_limiter)) : (j = i if speed == 0)
 }.each { |i| print(i, ' ') if i != j && ((c % 2 == 0) || (i != k)) }
 
 # python style
+#number_speeds = gets.to_i
 #(1..number_speeds).each do |i|
 #  speed = gets.to_i
 #  if speed % 2 != 0
